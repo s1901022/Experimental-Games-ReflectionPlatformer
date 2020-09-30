beea525fe1f4e1d5f61195747c4901fc12e19025
s1901022 <55756736+s1901022@users.noreply.github.com>
2020-09-30 18:23:32 +0100

    Reflection Framework
    Includes - Player gravity flipping mechanic and line of reflection level mirroring.

---
+++ Mirror Man/Assets/scrPlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrPlayerMovement : MonoBehaviour
+{
+    public int jumplimit;
+    private int extraJumps;
+
+    public float moveSpeed;
+    public float jumpForce;
+    private float moveInput;
+
+    public bool facingRight = true;
+    private bool grounded = false;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask ground;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = jumplimit;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, ground);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if ((facingRight == false && moveInput > 0) || (facingRight == true && moveInput < 0))
+        {
+            animationControl();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (grounded == true)
+        {
+            extraJumps = jumplimit;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && grounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void animationControl()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
---
+++ Mirror Man/Assets/scrSwitchGravity.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrSwitchGravity : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private scrPlayerMovement player;
+    private bool top;
+    public GameObject mirror;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<scrPlayerMovement>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = Vector3.Reflect(transform.position, mirror.transform.up);
+            //transform.position = new Vector3(transform.position.x, transform.position.y * -1, transform.position.z);
+            rb.gravityScale *= -1;
+            rotation();
+        }
+    }
+
+    void rotation()
+    {
+        if (top == false)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180f);
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        player.facingRight = !player.facingRight;
+        player.jumpForce = player.jumpForce * -1;
+        top = !top;
+    }
+}
