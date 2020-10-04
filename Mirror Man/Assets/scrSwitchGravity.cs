dcbb91b4d502854f6e58d29b8e12c9371f98b5f9
s1901022 <55756736+s1901022@users.noreply.github.com>
2020-10-04 16:37:17 +0100

    Added a reflection of the player so you know where you are flipping

--- Mirror Man/Assets/scrSwitchGravity.cs
+++ Mirror Man/Assets/scrSwitchGravity.cs
@@ -13,7 +13,9 @@ public class scrSwitchGravity : MonoBehaviour
     public float height;
     public Transform reflectNormal;
     public float checkFlip;
-    RaycastHit2D hitReflective;   
+    RaycastHit2D hitReflective;
+
+    public GameObject playerReflection;
 
     // Start is called before the first frame update
     void Start()
@@ -26,61 +28,90 @@ public class scrSwitchGravity : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+
         if (player.grounded)
         {
             canFlip = true;
         }
         if (Input.GetKeyDown(KeyCode.Space) && canFlip)
         {
-            rb.velocity = new Vector2(0.0f, 0.0f);
-            canFlip = false;
-            if (checkFlip == 1f)
-            {
-                hitReflective = Physics2D.Raycast(transform.position - new Vector3(0f, transform.localScale.y, 0f), Vector2.down);
-            }
-            else if (checkFlip == -1f)
-            {
-                hitReflective = Physics2D.Raycast(transform.position + new Vector3(0f, transform.localScale.y, 0f), Vector2.down);
-            }
-            reflectNormal = hitReflective.transform;
-            Vector2 size = hitReflective.collider.gameObject.GetComponent<BoxCollider2D>().bounds.size;
-            height = size.y;
-            Debug.Log("h: " + height);
-
-            if (hitReflective.collider != null && hitReflective.collider.tag == "Reflective")
-            {
-                float distanceBetweenReflection = transform.position.y - reflectNormal.position.y;
-                float reflectiveScale = height / 2;
-
-                if (transform.position.y > reflectNormal.position.y)
-                {
-                    transform.position = new Vector3(transform.position.x, (reflectNormal.position.y - reflectiveScale) - distanceBetweenReflection, transform.position.z);
-                }
-                else if (transform.position.y < reflectNormal.position.y)
-                {
-                    transform.position = new Vector3(transform.position.x, (reflectNormal.position.y + reflectiveScale) - distanceBetweenReflection, transform.position.z);
-                }
-
-                Debug.Log("First check");
-                Debug.Log(reflectNormal.position.y);
-                Debug.Log("dist " + distanceBetweenReflection);
-
-                checkFlip *= -1;
-                rb.gravityScale *= -1;
-                rotation();
-            }
-            else if (player.grounded == false)
-            {
-                Debug.Log("Nope goodbye");
-                checkFlip *= -1;
-                rb.gravityScale *= -1;
-                rotation();
-            }
-        }        
+            MirrorPlayer();  
+        }
+        UpdateReflection();
     }
 
-    void rotation()
+    void DetectTerrain()
+    {
+        if (checkFlip == 1f)
+        {
+            hitReflective = Physics2D.Raycast(transform.position - new Vector3(0f, transform.localScale.y, 0f), Vector2.down);
+        }
+        else if (checkFlip == -1f)
+        {
+            hitReflective = Physics2D.Raycast(transform.position + new Vector3(0f, transform.localScale.y + 1, 0f), Vector2.down);
+        }
+        reflectNormal = hitReflective.transform;
+    }
+
+    void MirrorPlayer()
+    {
+        rb.velocity = new Vector2(0.0f, 0.0f);
+        canFlip = false;
+
+        DetectTerrain();
+
+        if (hitReflective.collider != null && hitReflective.collider.tag == "Reflective")
+        {
+            float distanceBetweenReflection = transform.position.y - reflectNormal.position.y;
+            float reflectiveScale = reflectNormal.localScale.y / 2;
+
+            if (transform.position.y > reflectNormal.position.y)
+            {
+                transform.position = new Vector3(transform.position.x, ((reflectNormal.position.y - transform.localScale.y) + reflectiveScale) - distanceBetweenReflection, transform.position.z);
+            }
+            else if (transform.position.y < reflectNormal.position.y)
+            {
+                transform.position = new Vector3(transform.position.x, ((reflectNormal.position.y + transform.localScale.y) - reflectiveScale) - distanceBetweenReflection, transform.position.z);
+            }
+            checkFlip *= -1;
+            rb.gravityScale *= -1;
+            Rotation();
+        }
+        else if (player.grounded == false)
+        {
+            Debug.Log("Nope goodbye");
+            checkFlip *= -1;
+            rb.gravityScale *= -1;
+            Rotation();
+        }
+    }
+
+    void UpdateReflection()
+    {
+        DetectTerrain();
+        if (hitReflective.collider != null && hitReflective.collider.tag == "Reflective")
+        {
+            playerReflection.SetActive(true);
+            float distanceBetweenReflection = transform.position.y - reflectNormal.position.y;
+            float reflectiveScale = reflectNormal.localScale.y/2;
+
+            if (transform.position.y > reflectNormal.position.y)
+            {
+                playerReflection.transform.position = new Vector3(transform.position.x, ((reflectNormal.position.y + playerReflection.transform.localScale.y) - reflectiveScale) - distanceBetweenReflection, transform.position.z);
+            }
+            else if (transform.position.y < reflectNormal.position.y)
+            {
+                playerReflection.transform.position = new Vector3(transform.position.x, ((reflectNormal.position.y + playerReflection.transform.localScale.y) - reflectiveScale) - distanceBetweenReflection, transform.position.z);
+            }           
+        }
+        else if (player.grounded == false)
+        {
+            playerReflection.SetActive(false);
+        }
+    }
+
+    void Rotation()
     {
         if (top == false)
         {
