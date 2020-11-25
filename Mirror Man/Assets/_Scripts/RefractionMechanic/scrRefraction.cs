49b105b9786c2b38d9955a54b3dae8c4e3b605b1
BUISCUI7MUNCHER <34063887+BUISCUI7MUNCHER@users.noreply.github.com>
2020-11-25 18:38:35 +0000

    Added Olis work

---
+++ Mirror Man/Assets/_Scripts/RefractionMechanic/scrRefraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrRefraction : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    // New Split Position
+    private Vector2 splitPositionOne;
+    private Vector2 splitPositionTwo;
+    [SerializeField] 
+    private GameObject[] splitEmpty =  new GameObject[2];
+
+    [SerializeField]
+    GameObject playerTopHalf, PlayerBottomHalf;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        splitPositionOne = splitEmpty[0].gameObject.transform.position;
+        splitPositionTwo = splitEmpty[1].gameObject.transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "")
+        {
+            return;
+        }
+        else
+        {
+            SplitPlayer();
+        }
+    }
+
+    void SplitPlayer()
+    {
+        
+    }
+}
