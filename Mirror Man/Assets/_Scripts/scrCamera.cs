7f9494f62c7a70dae390f33d1df124cd9c3f5860
Oli Blackwell <34063887+BUISCUI7MUNCHER@users.noreply.github.com>
2021-03-29 11:48:08 +0100

    camera update

--- Mirror Man/Assets/_Scripts/scrCamera.cs
+++ Mirror Man/Assets/_Scripts/scrCamera.cs
@@ -2,50 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scrCamera : MonoBehaviour
-{
-    private GameObject player;
+public class scrCamera : MonoBehaviour {
+	[SerializeField]
+	Transform target;
 
-    [SerializeField]
-    GameObject[] borders = new GameObject[4];
+	float smoothSpeed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        borders[0] = GameObject.FindGameObjectWithTag("BarrierTop");
-        borders[1] = GameObject.FindGameObjectWithTag("BarrierBottom");
-        borders[2] = GameObject.FindGameObjectWithTag("BarrierLeft");
-        borders[3] = GameObject.FindGameObjectWithTag("BarrierRight");
-    }
+	Vector3 offset = new Vector3(0, 0, -6);
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 playerPosition = player.transform.position;
-        Vector3 cameraPosition = this.gameObject.transform.position;
+	private void Start() {
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
 
-        float halfScreenWidth = 9;
-        float halfScreenHeight = Screen.height / 2;
-
-        // X-AXIS
-        if (((playerPosition.x <= cameraPosition.x - halfScreenWidth) || (playerPosition.x >= cameraPosition.x + halfScreenWidth))
-            && ((cameraPosition.x - halfScreenWidth <= borders[3].transform.position.x) || (cameraPosition.x + halfScreenWidth >= borders[2].transform.position.x))) {
-            // Set the cameras position to move towards the players x position
-            gameObject.transform.position = Vector3.MoveTowards(transform.position,
-                                                                new Vector3(playerPosition.x, transform.position.y, transform.position.z),
-                                                                Time.deltaTime * 4.0f);
-        } else {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        }
-
-        // Y-AXIS
-        if (((playerPosition.y <= cameraPosition.y - halfScreenHeight) || (playerPosition.y >= cameraPosition.y + halfScreenHeight))
-            && ((cameraPosition.y - halfScreenHeight <= borders[0].transform.position.y) || (cameraPosition.y + halfScreenHeight >= borders[1].transform.position.y))) {
-            // Set the cameras position to move towards the players x position
-            gameObject.transform.position = Vector3.MoveTowards(transform.position,
-                                                                new Vector3(transform.position.x, playerPosition.y, transform.position.z),
-                                                                Time.deltaTime * 2.5f);
-        }
-    }
+	void LateUpdate() {
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPosition;
+	}
 }
